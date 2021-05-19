232d51d23ff438816e25a047a310ac78da582f1d
JShawnDunn <jshawndunn@gmail.com>
2021-05-19 11:30:25 -0400

    create route to delete card from deck

--- controllers/decks.js
+++ controllers/decks.js
@@ -29,14 +29,22 @@ router.get('/remove/:id', isLoggedIn, async (req, res) => {
 
 router.get('/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params
-  const deck = await db.deck.findOne({
-      where: {
-          id
-      },
-      include: [db.card]
-  })
+  const deck = await db.deck.findOne({ where: { id }, include: [db.card] })
   
   res.render("decks/show", { deck })
+})
+
+router.get('/card/:deckId/:cardId', isLoggedIn, async (req, res) => {
+  // params passed in from decks/display
+  const { deckId, cardId } = req.params
+  // grab deck from model on deckId
+  const deck = await db.deck.findOne({ where: { id:deckId }})
+  // grab card from model on cardId
+  const card = await db.card.findOne({ where: { id:cardId }})
+  // use the association helper REMOVE to, well, remove the association from deck to the card instance 
+  deck.removeCard(card)
+
+  res.redirect(`/decks/${deckId}`)
 })
 
 router.post('/new', isLoggedIn, async (req,res) => {

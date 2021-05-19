76811d92f8ade9fee51c0f6648062080e216bc38
JShawnDunn <jshawndunn@gmail.com>
2021-05-19 15:15:23 -0400

    organize route code. add comments

--- controllers/decks.js
+++ controllers/decks.js
@@ -6,60 +6,51 @@ const isLoggedIn = require('../middleware/isLoggedIn');
 const db = require('../models');
 
 // ROUTES GET
+// Displays all decks
 router.get('/', isLoggedIn, async (req, res) => {
   const { id } = req.user.get(); 
-  const decks = await db.deck.findAll({
-      where: {
-          userId:id
-      }
-  });
-  res.render('decks/display', { decks })
+  const decks = await db.deck.findAll({ where: { userId:id } });
+  res.render('decks/display', { decks });
 })
-  
+// Create New Deck Form  
 router.get('/new', isLoggedIn, (req, res) => {
-  res.render('decks/createForm')
+  res.render('decks/createForm');
 })
-
+// Display all cards belonging to a deck
 router.get('/:id', isLoggedIn, async (req, res) => {
-  const { id } = req.params
-  const deck = await db.deck.findOne({ where: { id }, include: [db.card] })
-  
-  res.render("decks/show", { deck })
+  const { id } = req.params;
+  const deck = await db.deck.findOne({ where: { id }, include: [db.card] }); 
+  res.render("decks/show", { deck });
 })
 
 // ROUTES POST
+// Create new deck
 router.post('/new', isLoggedIn, async (req,res) => {
   const { id } = req.user.get();
-  const { deckName } = req.body
-  const user  = await db.user.findOne({
-    where: {
-      id:id
-    }
-  })
-  user.createDeck({
-    name: deckName
-  })
+  const { deckName } = req.body;
+  const user  = await db.user.findOne({ where:{ id:id } });
+  user.createDeck({ name:deckName });
   res.redirect('/decks')
 })
 
 // ROUTES PUT
+// Update deck name
 router.put('/name', isLoggedIn, async (req,res) => {
   const { id, name } = req.body;
   const deck = await db.deck.findOne({ where: { id }});
-  deck.update({ name })
+  await deck.update({ name })
   res.redirect(`/decks/${id}`)
 })
 
 // ROUTES DELETE
+// Delete Entire Deck
 router.delete('/remove/:id', isLoggedIn, async (req, res) => {
   const { id } = req.params
   await db.deck.destroy({ where: { id } })
-
   res.redirect('/decks')
 })
 
-
-// route to delete a single card from a deck. 
+// Delete a single card from a deck. 
 router.delete('/card/:deckId/:cardId', isLoggedIn, async (req, res) => {
   // params passed in from view decks/display
   const { deckId, cardId } = req.params
@@ -68,7 +59,7 @@ router.delete('/card/:deckId/:cardId', isLoggedIn, async (req, res) => {
   // grab card from model on cardId
   const card = await db.card.findOne({ where: { id:cardId }})
   // use the association helper REMOVE to, well, remove the association from deck to the card instance 
-  deck.removeCard(card)
+  await deck.removeCard(card)
   // redirect(we don't want their URL on /card/:deckId/:cardId) to show cards in deck.
   res.redirect(`/decks/${deckId}`)
 })
--- controllers/search.js
+++ controllers/search.js
@@ -6,30 +6,31 @@ const mtg = require('mtgsdk')
 
 const db = require('../models');
 
+// GET ROUTES
+// Display search form
 router.get('/', isLoggedIn, (req, res) => {
     res.render('search/search')
 })
-
+// POST ROUTES
+// Display search results
 router.post('/results', isLoggedIn, async (req, res) => {
     const { card } = req.body 
     const cards = await mtg.card.where({ name: card })
     res.render('search/results', { cards })
 })
-
+// Insert card into database and connect to a deck
 router.post('/add', isLoggedIn, async (req, res) => {
     const { name, id } = req.body;
-    const deck = await db.deck.findOne({
-        where: { name }
-    });
+    const deck = await db.deck.findOne({ where: { name } });
     const card = await mtg.card.find(id);
-
+    // Inserting data for card
     const [addedCard] = await db.card.findOrCreate({
         where: { name:card.card.name,
             set:card.card.set,
             imgUrl:card.card.imageUrl
         }
     })
-    deck.addCard(addedCard)
+    await deck.addCard(addedCard)
     res.redirect('/search')
 })
 module.exports = router;
